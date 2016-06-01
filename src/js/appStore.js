49f0298b6038cd04fb34d46d35854cbbf13e2a93
Robert Winterbottom <rwinterbottom@blueraster.com>
2016-06-01 09:55:31 -0400

    Adding first build

--- src/js/appStore.js
+++
@@ -1,5 +0,0 @@
-import appReducer from 'js/reducers/appReducer';
-import {createStore} from 'redux';
-
-// export a singleton store object
-export default createStore(appReducer);
--- src/js/constants/actionTypes.js
+++
@@ -1,4 +0,0 @@
-/* @flow */
-export const VIEW_READY:string = 'VIEW_READY',
-             SHOW_LOCATE:string = 'SHOW_LOCATE',
-             SHOW_SHARE:string = 'SHOW_SHARE';
