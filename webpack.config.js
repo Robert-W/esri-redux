2663b87368447a532caa742c1a2ab2e104c6b391
Robert Winterbottom <rwinterbottom@blueraster.com>
2016-06-27 10:41:18 -0400

    Cleaned up config and fixed issue with CSP in firefox

--- webpack.config.js
+++ webpack.config.js
@@ -1,29 +1,14 @@
 /* eslint-disable */
+var production = process.env.NODE_ENV === 'production';
 var helpers = require('./webpack.helpers');
 var webpack = require('webpack');
 var path = require('path');
 
-module.exports = {
-  debug: true,
-  cache: true,
-  devtool: 'source-map',
-  entry: [
-    'webpack/hot/dev-server',
-    'webpack-hot-middleware/client',
-    path.join(__dirname, 'src/js/main')
-  ],
-  output: {
-    path: path.resolve('build/js'),
-    filename: 'main.js',
-    publicPath: '/js/',
-    libraryTarget: 'amd'
-  },
+var config = {
+  externals: [helpers.esriPackages],
+  resolve: { alias: helpers.modules },
   module: {
     loaders: [{
-      test: /\.js?$/,
-      loader: 'react-hot',
-      exclude: /(node_modules|build)/
-    }, {
       test: /\.js?$/,
       loader: 'babel',
       exclude: /(node_modules|build)/,
@@ -32,11 +17,56 @@ module.exports = {
         plugins: ['transform-runtime', 'babel-plugin-transform-flow-strip-types']
       }
     }]
-  },
-  externals: [helpers.esriPackages],
-  resolve: { alias: helpers.modules },
-  plugins: [
+  }
+};
+
+if (production) {
+  // Setup for production
+  // Configure entry
+  config.entry = path.join(__dirname, 'src/js/main');
+  // Configure output and public path for hot module
+  config.output = {
+    path: path.resolve('dist/js'),
+    filename: 'main.js',
+    libraryTarget: 'amd'
+  };
+  // Add Environment variables via plugins
+  config.plugins = [
+    new webpack.DefinePlugin({
+      'process.env': {
+        'NODE_ENV': '"production"'
+      }
+    })
+  ];
+} else {
+  // Setup for development
+  config.debug = true;
+  config.cache = true;
+  config.devtool = 'source-map';
+  // Configure entry
+  config.entry = [
+    'webpack/hot/dev-server',
+    'webpack-hot-middleware/client',
+    path.join(__dirname, 'src/js/main')
+  ];
+  // Configure output and public path for hot module
+  config.output = {
+    path: path.resolve('build/js'),
+    filename: 'main.js',
+    publicPath: '/js/',
+    libraryTarget: 'amd'
+  };
+  // Add the hot loader as the first loader
+  config.module.loaders.unshift({
+    test: /\.js?$/,
+    loader: 'react-hot',
+    exclude: /(node_modules|build)/
+  });
+  // Add some plugins
+  config.plugins = [
     new webpack.HotModuleReplacementPlugin(),
     new webpack.NoErrorsPlugin()
-  ]
-};
+  ];
+}
+
+module.exports = config;
