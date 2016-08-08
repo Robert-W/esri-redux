f0317cd8d21672072fa62486113d99106af62eb0
Robert Winterbottom <rwinterbottom@blueraster.com>
2016-08-07 23:17:32 -0400

    Updates to flowtypes

--- declarations/EsriTypes.js
+++ declarations/EsriTypes.js
@@ -1,8 +1,16 @@
+/*eslint-disable */
 declare type EsriView = Object | {
   goTo: () => void,
   zoom: number,
   ready: bool
 };
+
+declare module 'esri/Map' {
+  declare class Map {
+    constructor(options:Object): void;
+  }
+  declare var exports: Class<Map>;
+}
 
 declare module 'esri/request' {
   declare function exports(url: string, options: Object): Promise<Object>;
