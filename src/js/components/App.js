ff5d673f0b019c445a1ad4b16ec36986cdd42af2
Whitney Purdum <wpurdum@costar.com>
2019-09-03 12:17:55 -0400

    remove unused imports from App js and import Main container

--- src/js/components/App.js
+++ src/js/components/App.js
@@ -1,8 +1,6 @@
-// import MapView from 'js/components/MapView';
-// import Header from 'js/components/Header';
+
 import Main from '../containers/Main';
 import React, { Component } from 'react';
-// import { TEXT } from 'js/config';
 
 export default class App extends Component {
   displayName: 'App';
@@ -11,8 +9,6 @@ export default class App extends Component {
     return (
       <div className='root'>
         <Main />
-        {/* <Header title={TEXT.title} subtitle={TEXT.subtitle} />
-        <MapView /> */}
       </div>
     );
   }
